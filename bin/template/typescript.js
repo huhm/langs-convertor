f099639fc1e02765c568c3a87a89ab22e7f8c08a
huhm <huhm2008@foxmail.com>
2023-05-19 14:29:09 +0800

    feat: import type语法

--- bin/template/typescript.js
+++ bin/template/typescript.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const TS_TEMPLATE_TYPE_NAME = "LANGINFO_{{=(it.moduleName||'ROOT').toUpperCase()}}";
-const TS_TEMPLATE_MODULE_NAME = "LangInfo{{!it.moduleName}}";
+const TS_TEMPLATE_MODULE_NAME = 'LangInfo{{!it.moduleName}}';
 const TS_TEMPLATE = `/* eslint-disable prettier/prettier */
-{{? it.langName.toLowerCase()!=='en'}}import { ${TS_TEMPLATE_MODULE_NAME} } from './en'{{?}}
-const ${TS_TEMPLATE_TYPE_NAME}{{? it.langName.toLowerCase()!=='en'}}:${TS_TEMPLATE_MODULE_NAME}{{?}} = {{=it.moduleJsonString}}
+{{? it.langName.toLowerCase()!=='en'}}import type { ${TS_TEMPLATE_MODULE_NAME} } from './en'{{?}}
+const ${TS_TEMPLATE_TYPE_NAME}{{? it.langName.toLowerCase()!=='en'}}:Partial<${TS_TEMPLATE_MODULE_NAME}>{{?}} = {{=it.moduleJsonString}}
 {{?it.langName.toLowerCase()==='en'}}export type ${TS_TEMPLATE_MODULE_NAME} = typeof ${TS_TEMPLATE_TYPE_NAME}{{?}}
 export default ${TS_TEMPLATE_TYPE_NAME}
 `;
--- src/template/typescript.ts
+++ src/template/typescript.ts
@@ -1,9 +1,9 @@
-
-const TS_TEMPLATE_TYPE_NAME="LANGINFO_{{=(it.moduleName||'ROOT').toUpperCase()}}"
-const TS_TEMPLATE_MODULE_NAME="LangInfo{{!it.moduleName}}"
-const TS_TEMPLATE=`/* eslint-disable prettier/prettier */
-{{? it.langName.toLowerCase()!=='en'}}import { ${TS_TEMPLATE_MODULE_NAME} } from './en'{{?}}
-const ${TS_TEMPLATE_TYPE_NAME}{{? it.langName.toLowerCase()!=='en'}}:${TS_TEMPLATE_MODULE_NAME}{{?}} = {{=it.moduleJsonString}}
+const TS_TEMPLATE_TYPE_NAME =
+  "LANGINFO_{{=(it.moduleName||'ROOT').toUpperCase()}}"
+const TS_TEMPLATE_MODULE_NAME = 'LangInfo{{!it.moduleName}}'
+const TS_TEMPLATE = `/* eslint-disable prettier/prettier */
+{{? it.langName.toLowerCase()!=='en'}}import type { ${TS_TEMPLATE_MODULE_NAME} } from './en'{{?}}
+const ${TS_TEMPLATE_TYPE_NAME}{{? it.langName.toLowerCase()!=='en'}}:Partial<${TS_TEMPLATE_MODULE_NAME}>{{?}} = {{=it.moduleJsonString}}
 {{?it.langName.toLowerCase()==='en'}}export type ${TS_TEMPLATE_MODULE_NAME} = typeof ${TS_TEMPLATE_TYPE_NAME}{{?}}
 export default ${TS_TEMPLATE_TYPE_NAME}
 `
